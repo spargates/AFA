46f9ab7447c8c7d7fa8eb0f384de87cb80bc94c5
julian.pallares <spargates@gmail.com>
2024-06-03 22:27:16 +0200

    Redirect

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+ async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://test.afabenadressa.org',
+        permanent: false, // or true for a 308 permanent redirect
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
